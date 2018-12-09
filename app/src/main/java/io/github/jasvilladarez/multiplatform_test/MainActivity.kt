e3b5e2e5b08a5af558c14ebfc1650c99aedba260
Jasmine Villadarez <annejasminevilladarez@gmail.com>
2018-12-09 13:43:23 +0100

    Update android code to display result of shared code

--- app/src/main/java/io/github/jasvilladarez/multiplatform_test/MainActivity.kt
+++ app/src/main/java/io/github/jasvilladarez/multiplatform_test/MainActivity.kt
@@ -2,11 +2,14 @@ package io.github.jasvilladarez.multiplatform_test
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.github.jasvilladarez.multiplatform_test_shared.createAppMessage
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test.text = createAppMessage()
     }
 }
